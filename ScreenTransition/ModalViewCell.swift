6aa75cb8e0c3bdc5f2da43589a200bbb55622ff9
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-09 21:24:32 +0900

    screenTransition

---
+++ ScreenTransition/ModalViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ModalViewCell.swift
+//  ScreenTransition
+//
+//  Created by 中田誠 on 2022/12/09.
+//
+
+import UIKit
+
+class ModalViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
